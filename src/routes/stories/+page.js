88f65548408b0e1ddd574187453c2f4697f5241f
nicucalcea <mail@nicu.md>
2025-07-21 21:28:31 +0100

    server-side rendering

--- src/routes/stories/+page.js
+++ src/routes/stories/+page.js
@@ -1,5 +1,9 @@
 import cvData from '../../data/cv.json';
 
+export const prerender = true;
+export const ssr = true;
+export const csr = false;
+
 export function load() {
   // Return all publications sorted chronologically (latest first)
   const sortedPublications = cvData.publications
